8ed0d3791cf2c5df917946e18f492e2d33730d7b
Tusharg1999 <tushargoyal414@gmail.com>
2020-06-20 23:50:55 +0530

    initialize project

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import './views/Home.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Wally",
+      theme: ThemeData(
+        primaryColor: Colors.white,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
